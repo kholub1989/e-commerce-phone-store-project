bd03c10b0a0548f4f46a129d7d2c6dbde6557f6d
Krisztian Holub <kholub1989@gmail.com>
2020-10-10 12:14:05 -0400

    add context to get all products data

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
 import { storeProducts } from "../data";
+import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
   state = {
@@ -9,17 +10,23 @@ export default class ProductList extends Component {
   };
 
   render() {
-    console.log(this.state.products);
     return (
       <React.Fragment>
         <div className="py-5">
           <div className="container">
             <Title name="our" title="product" />
-            <div className="row"></div>
+            <div className="row">
+              <ProductConsumer>
+                {(value) => {
+                  return value.products.map((product) => {
+                    return <Product key={product.id} product={product} />;
+                  });
+                }}
+              </ProductConsumer>
+            </div>
           </div>
         </div>
       </React.Fragment>
-      // <Product />
     );
   }
 }
