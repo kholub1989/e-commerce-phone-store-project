812cd535a86bbee41d003bf9206645936d21ae0e
Krisztian Holub <kholub1989@gmail.com>
2020-10-12 16:31:32 -0400

    addToCart functionality

--- src/context.js
+++ src/context.js
@@ -7,6 +7,7 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct,
+    cart: [],
   };
 
   componentDidMount() {
@@ -36,7 +37,21 @@ class ProductProvider extends Component {
   };
 
   addToCart = (id) => {
-    console.log(`hello from add to cart when id is ${id}`);
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+    this.setState(
+      () => {
+        return { products: tempProducts, cart: [...this.state.cart, product] };
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
   };
 
   render() {
